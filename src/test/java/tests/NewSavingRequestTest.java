eea297122d7cc4e301df332602f1e545ad9ebf99
martin.skarbala <skarbala.martin@gmail.com>
2019-07-30 10:16:07 +0200

    add test to check total income in new detail

---
+++ src/test/java/tests/NewSavingRequestTest.java
@@ -0,0 +1,35 @@
+package tests;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+
+import base.TestBase;
+import pages.CalculatorPage;
+
+public class NewSavingRequestTest extends TestBase {
+
+    @Test
+    public void itShouldDisplayTotalIncomeInNewRequest() {
+        CalculatorPage calculatorPage = new CalculatorPage(driver);
+        calculatorPage.selectFund("Tom & Jerry corp");
+        calculatorPage.enterOneTimeInvestment("2820");
+        calculatorPage.enterYears("15");
+        calculatorPage.enterEmail("info@furbo.sk");
+        //precitat zo stranky total income
+        String calculatedIncome = calculatorPage.getTotalIncome();
+        //vytvorit novy saving request
+        driver.findElement(By.cssSelector("button.btn-block")).click();
+        //overim ze total income sa zobrazi v requeste
+
+        Assert.assertEquals(
+            calculatedIncome,
+            driver
+                .findElement(By.xpath("//ul[contains(@class,'saving-list')]/li//div[contains(@class,'amounts')" +
+                    "]/p/span"))
+                .getText()
+        );
+
+    }
+
+}
